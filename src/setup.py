31ccb2d6fc27448b2aa4b2f62f4fcf0d155ae2e1
Andrew Gordon <65138166+andrewgg1@users.noreply.github.com>
2024-11-20 17:32:25 -0500

    test1

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dedreira-awesomecalculator", # Replace with your own username
+    name="Group3-awesomecalculator", # Replace with your own username
     version= "1.0.0",
     author="Daniel Edreira",
     author_email="dedreira@github.com",
@@ -12,11 +12,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dedreira/awesomecalculator",
+    fork="https://github.com/andrewgg1/awesomecalculator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
